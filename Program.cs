a851ae10c5d1024f21cf90baf04785d61437f170
Zakarea <98216446+Zakarea79@users.noreply.github.com>
2025-02-08 19:16:47 +0000

    fix bug

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using API;
+using Newtonsoft.Json;
 
 TelegramBotApi telegramBotApi = new TelegramBotApi();
-telegramBotApi.Token = "";
-
+telegramBotApi.Token = "7487389693:AAH5hfhVBNh0EWC4Hkb6ipcfagzHKbjWbPQ";
 AiApi ai_api = new AiApi();
+
+Console.WriteLine("Start Bot ... ");
 
 dynamic offset = 0;
 while (true)
 {
     dynamic t_data = telegramBotApi.getUpdate(offset);
+    t_data = JsonConvert.DeserializeObject<dynamic>(t_data);
     foreach (var item in t_data["result"])
     {
         if (item["message"]["text"] != "" && item["message"]["text"] != null)
@@ -19,6 +22,10 @@ while (true)
                 telegramBotApi.sendMessage(from_ai, item["message"]["chat"]["id"]);
                 Console.WriteLine(item["message"]["chat"]["id"]);
             });
+            offset += item["update_id"] + 1;
+        }
+        else
+        {
             offset += item["update_id"] + 1;
         }
     }
--- obj/Debug/net8.0/chat-bot-ai.AssemblyInfo.cs
+++ obj/Debug/net8.0/chat-bot-ai.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat-bot-ai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f577152260bf7f9c3beede73e6baa37a5400e60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ebcab25fd125453770ac5c0ae1e7626b7d6aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat-bot-ai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat-bot-ai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
